48f7c4b0ec570b5bb3dce92467a48ada974b6110
Josue Castro <jcame@food.ku.dk>
2021-11-29 14:31:56 +0100

    Update readme.sh

--- bin/readme.sh
+++ bin/readme.sh
@@ -1,2 +1,9 @@
-bin/
-contains XXX
+# bin/
+
+## Modified DB of Viral Orthologous Groups (VOGs | https://vogdb.org/) + proteins from Anelloviruses (Circoviridae) derived from our recent pre-print Shah et al. 2021 (https://doi.org/10.1101/2021.07.02.450849)
+
+## A DB of CRISPRs spacers and tRNAs predicted from species-level genome bins (SGBs) described in Pasolli et al. 2019 (https://doi.org/10.1016/j.cell.2019.01.001) & metagenome-assembled genomes (MAGs) described in Castro-Mejía et al. 2021 (https://doi.org/10.1101/2021.09.01.458531)
+
+## Nextera adaptors/transposases & PhiX Illumina control sequences
+
+
